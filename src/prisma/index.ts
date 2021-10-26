42759805698383d064f88bd5c4f31161c1ae5de2
Alessandro B. Cesta <54996931+Alessandro1918@users.noreply.github.com>
2021-10-26 17:14:55 -0300

    Delete src directory

--- src/prisma/index.ts
+++
@@ -1,7 +0,0 @@
-//Prisma client - Conexão com a db
-
-import { PrismaClient } from '@prisma/client'
-
-const prismaClient = new PrismaClient()
-
-export default prismaClient
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-/*import { httpServer } from './app'
-
-httpServer.listen(3333, () => {
-    console.log("Server is running on PORT 3333")
-})*/
